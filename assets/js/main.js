b97a778aad30632c180bfc742a3227046cbaf46a
Jordy <jordycorporan95@gmail.com>
2023-08-25 08:41:52 -0400

    Portfolio finalyzed

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,25 +28,25 @@ window.addEventListener("scroll", () => {
 });
 
 // activate nav link on scroll
-function addActiveLink() {
-  const section = document.querySelectorAll("section[id]");
-  section.forEach((section) => {
-    const scrollY = window.scrollY,
-      sectionTop = section.offsetTop - 120,
-      sectionHeight = section.offsetHeight,
-      sectionId = section.getAttribute("id");
+// function addActiveLink() {
+//   const section = document.querySelectorAll("section[id]");
+//   section.forEach((section) => {
+//     const scrollY = window.scrollY,
+//       sectionTop = section.offsetTop - 120,
+//       sectionHeight = section.offsetHeight,
+//       sectionId = section.getAttribute("id");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__link[href*=" + sectionId + "]")
-        .classList.add("nav__link--active");
-    } else {
-      document
-        .querySelector(".nav__link[href*=" + sectionId + "]")
-        .classList.remove("nav__link--active");
-    }
-  });
-}
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".nav__link[href*=" + sectionId + "]")
+//         .classList.add("nav__link--active");
+//     } else {
+//       document
+//         .querySelector(".nav__link[href*=" + sectionId + "]")
+//         .classList.remove("nav__link--active");
+//     }
+//   });
+// }
 
 window.addEventListener("scroll", addActiveLink);
 
